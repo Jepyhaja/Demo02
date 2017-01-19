4c909a620ac0373ddc3709778b3ea2234983ed2e
Jere <jepyhaja@gmail.com>
2017-01-19 13:14:13 +0200

    Fixed salute

--- first_test/Program.cs
+++ first_test/Program.cs
@@ -10,7 +10,7 @@ namespace first_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Hello GitHubbille!!");
         }
     }
 }
